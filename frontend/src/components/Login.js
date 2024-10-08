cad447a1faed0595eb4544ec1f652c3d39620a6a
{{tladuke32}} <{{tladuke32@gmail.com}}>
2024-10-08 16:31:17 -0400

    Attempt to fic api url issues.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -9,7 +9,7 @@ function Login({ onLogin }) {
         e.preventDefault();
 
         try {
-            const response = await fetch(`'${apiURL}/login`, {
+            const response = await fetch(`'${process.env.REACT_APP_API_URL}/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password }),
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -12,7 +12,7 @@ function Register() {
         setSuccessMessage(''); // Clear any previous messages
         setErrorMessage('');
         try {
-            const response = await fetch(`'${apiURL}/register`, { // Use the API URL and endpoint
+            const response = await fetch(`'${process.env.REACT_APP_API_URL}/register`, { // Use the API URL and endpoint
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password }),
